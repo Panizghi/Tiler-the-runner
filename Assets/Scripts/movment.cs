ca1b7be00954767f20a16e548ff3a40e172da8e8
Panizghi <90856064+Panizghi@users.noreply.github.com>
2022-09-20 19:40:06 -0400

    files

---
+++ Assets/Scripts/movment.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class movment : MonoBehaviour
+{
+    Vector2 moveInput;
+
+    Rigidbody2D myRigidbody;
+
+    void Start()
+    {
+        myRigidbody = GetComponnent<Rigidbody2D>;
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+   void OnMove(InputValue value){
+        // grab the input value and print it 
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+
+    void Run(){
+        Vector2 PlayerVelocity = new Vectro2 ( moveInput.x,0f);
+        myRigidbody.velocity = PlayerVelocity;
+    }
+}
